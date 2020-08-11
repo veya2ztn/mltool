7121af1d74bfd3bb3852772244e593eb5c93294a
veya2ztn <zhangtianning@gmail.com>
2020-08-11 21:21:53 +0800

    FPNTransConv2d

--- ModelArchi/TransConv2d.py
+++ ModelArchi/TransConv2d.py
@@ -102,3 +102,83 @@ class UpSampleResNet(nn.Module):
         x = self.finalpool(x)
 
         return x
+
+
+class FPNUpSample(nn.Module):
+    def __init__(self, block,layerconfig):
+        super().__init__()
+        block=block
+        channel_start,_,_ = layerconfig[0]
+        self.conv1   = nn.ConvTranspose2d(layerconfig[0][0], layerconfig[1][0], kernel_size=7)
+        self.bn1     = nn.BatchNorm2d(layerconfig[1][0])
+        self.relu    = nn.ReLU(inplace=True)
+        self.firstpool = nn.AdaptiveAvgPool2d(2)
+        self.inplanes = layerconfig[1][0]
+
+
+        # Bottom-up layers
+        self.layer2 = self._make_layer(block, layerconfig[1][0], layerconfig[1][1], stride=layerconfig[1][2])
+        self.layer3 = self._make_layer(block, layerconfig[2][0], layerconfig[2][1], stride=layerconfig[2][2])
+        self.layer4 = self._make_layer(block, layerconfig[3][0], layerconfig[3][1], stride=layerconfig[3][2])
+        self.layer5 = self._make_layer(block, layerconfig[4][0], layerconfig[4][1], stride=layerconfig[4][2])
+
+        self.conv6 = nn.Conv2d(layerconfig[4][0], layerconfig[1][0], kernel_size=3, stride=2, padding=1)
+        self.conv7 = nn.Conv2d(layerconfig[1][0], layerconfig[1][0], kernel_size=3, stride=2, padding=1)
+
+        # Top layer
+        self.toplayer = nn.Conv2d(layerconfig[4][0], layerconfig[1][0], kernel_size=1, stride=1, padding=0)  # Reduce channels
+
+        # Smooth layers
+        self.smooth1 = nn.Conv2d(layerconfig[1][0], layerconfig[1][0], kernel_size=3, stride=1, padding=1)
+        self.smooth2 = nn.Conv2d(layerconfig[1][0], layerconfig[1][0], kernel_size=3, stride=1, padding=1)
+
+        # Lateral layers
+        self.latlayer1 = nn.Conv2d(layerconfig[3][0], layerconfig[1][0], kernel_size=1, stride=1, padding=0)
+        self.latlayer2 = nn.Conv2d(layerconfig[2][0], layerconfig[1][0], kernel_size=1, stride=1, padding=0)
+
+        self.finalpool = nn.AdaptiveAvgPool2d(16)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        layers = []
+        layers.append(block(self.inplanes, out_channels, stride))
+        self.inplanes = out_channels
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, out_channels, 1))
+        return nn.Sequential(*layers)
+
+    def _upsample_add(self, x, y):
+        '''Upsample and add two feature maps.
+        Args:
+          x: (Variable) top feature map to be upsampled.
+          y: (Variable) lateral feature map.
+        Returns:
+          (Variable) added feature map.
+        Note in PyTorch, when input size is odd, the upsampled feature map
+        with `F.upsample(..., scale_factor=2, mode='nearest')`
+        maybe not equal to the lateral feature map size.
+        e.g.
+        original input size: [N,_,15,15] ->
+        conv2d feature map size: [N,_,8,8] ->
+        upsampled feature map size: [N,_,16,16]
+        So we choose bilinear upsample which supports arbitrary output sizes.
+        '''
+        _,_,H,W = y.size()
+        return F.interpolate(x, size=(H,W), mode='bilinear',align_corners=True) + y
+
+    def forward(self, x):
+        # Bottom-up
+        c1 = self.firstpool(self.relu(self.bn1(self.conv1(x))))
+        c2 = self.layer2(c1)
+        c3 = self.layer3(c2)
+        c4 = self.layer4(c3)
+        c5 = self.layer5(c4)
+        p6 = self.conv6(c5)
+        p7 = self.conv7(F.relu(p6))
+        # Top-down
+        p5 = self.toplayer(c5)
+        p4 = self._upsample_add(p5, self.latlayer1(c4))
+        p3 = self._upsample_add(p4, self.latlayer2(c3))
+        # Smooth
+        p4 = self.smooth1(p4)
+        p3 = self.smooth2(p3)
+        return p3, p4, p5, p6, p7

41de8360e68cad7ca2a2a95148c8724aa91fcd0a
veya2ztn <zhangtianning@gmail.com>
2020-08-12 17:08:39 +0800

    GANmodel

--- ModelArchi/MyConv2d.py
+++ ModelArchi/MyConv2d.py
@@ -4,18 +4,18 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 
-class TransposeBottleneck(nn.Module):
+class Bottleneck(nn.Module):
     """ Adapted from torchvision.models.resnet """
 
     def __init__(self, in_channels, output_channels, stride,mid_channels=None,upsample=None, norm_layer=nn.BatchNorm2d):
         super().__init__()
         if mid_channels is None:mid_channels=in_channels
         optpad = stride-1
-        self.conv1    = nn.ConvTranspose2d(in_channels, mid_channels, kernel_size=1, bias=False)
+        self.conv1    = nn.Conv2d(in_channels, mid_channels, kernel_size=1, bias=False)
         self.bn1      = norm_layer(mid_channels)
-        self.conv2    = nn.ConvTranspose2d(mid_channels, mid_channels, kernel_size=3, stride=stride,padding=1,output_padding=optpad, bias=False)
+        self.conv2    = nn.Conv2d(mid_channels, mid_channels, kernel_size=3, stride=stride,padding=1, bias=False)
         self.bn2      = norm_layer(mid_channels)
-        self.conv3    = nn.ConvTranspose2d(mid_channels, output_channels, kernel_size=1, bias=False)
+        self.conv3    = nn.Conv2d(mid_channels, output_channels, kernel_size=1, bias=False)
         self.bn3      = norm_layer(output_channels)
         self.relu     = nn.ReLU(inplace=True)
         #self.relu  = nn.LeakyReLU(negative_slope=0.6, inplace=True)
@@ -24,12 +24,11 @@ class TransposeBottleneck(nn.Module):
         if self.upsample is None:
             if output_channels!=in_channels or stride >1:
                 self.upsample = nn.Sequential(
-                        nn.ConvTranspose2d(in_channels,
+                        nn.Conv2d(in_channels,
                                            output_channels,
                                            kernel_size=3,
                                            stride=stride,
                                            padding=1,
-                                           output_padding=optpad,
                                            bias=False) ,
                         norm_layer(output_channels),
                     )
@@ -55,26 +54,26 @@ class TransposeBottleneck(nn.Module):
 
         return out
 
-class TransposeBottleneckV0(TransposeBottleneck):
+class BottleneckV0(Bottleneck):
     def __init__(self, in_channels, output_channels, stride):
         mid_channels  = in_channels
         super().__init__(in_channels, output_channels, stride,mid_channels=mid_channels)
-class TransposeBottleneckV1(TransposeBottleneck):
+class BottleneckV1(Bottleneck):
     def __init__(self, in_channels, output_channels, stride):
         mid_channels  = output_channels
         super().__init__(in_channels, output_channels, stride,mid_channels=mid_channels)
-class TransposeBottleneckV2(TransposeBottleneck):
+class BottleneckV2(Bottleneck):
     def __init__(self, in_channels, output_channels, stride):
         mid_channels  = (in_channels+output_channels)//2
         super().__init__(in_channels, output_channels, stride,mid_channels=mid_channels)
 
 
-class UpSampleResNet(nn.Module):
+class ResNetConfig(nn.Module):
     def __init__(self, block,layerconfig):
         super().__init__()
         block=block
         channel_start,_,_ = layerconfig[0]
-        self.conv1   = nn.ConvTranspose2d(layerconfig[0][0], layerconfig[1][0], kernel_size=7)
+        self.conv1   = nn.Conv2d(layerconfig[0][0], layerconfig[1][0], kernel_size=7)
         self.bn1     = nn.BatchNorm2d(layerconfig[1][0])
         self.relu    = nn.ReLU(inplace=True)
         self.firstpool = nn.AdaptiveAvgPool2d(2)
@@ -109,7 +108,7 @@ class FPNUpSample(nn.Module):
         super().__init__()
         block=block
         channel_start,_,_ = layerconfig[0]
-        self.conv1   = nn.ConvTranspose2d(layerconfig[0][0], layerconfig[1][0], kernel_size=7)
+        self.conv1   = nn.Conv2d(layerconfig[0][0], layerconfig[1][0], kernel_size=7)
         self.bn1     = nn.BatchNorm2d(layerconfig[1][0])
         self.relu    = nn.ReLU(inplace=True)
         self.firstpool = nn.AdaptiveAvgPool2d(2)

7d3bc1438b110153029bfa2e77c85458c3c1ed40
veya2ztn <zhangtianning110@gmail.com>
2021-04-12 15:32:08 +0800

    add Origin(but without dropout) DARTS model

---
+++ ModelArchi/ModelSearch/DARTS_Model_Origin.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+from .operations_define import *
+from .operations_define import DARTS_ORIGIN_OPS as OPS
+from torch.autograd import Variable
+
+
+def drop_path(x, drop_prob):
+    if drop_prob > 0.0:
+        keep_prob = 1.0 - drop_prob
+        mask = Variable(
+            torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob)
+        )
+        x.div_(keep_prob)
+        x.mul_(mask)
+    return x
+
+class Cell(nn.Module):
+    def __init__(self, genotype, C_prev_prev, C_prev, C, reduction, reduction_prev):
+        super(Cell, self).__init__()
+        print(C_prev_prev, C_prev, C)
+
+        if reduction_prev:
+            self.preprocess0 = FactorizedReduce(C_prev_prev, C)
+        else:
+            self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0)
+        self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0)
+
+        if reduction:
+            op_names, indices = zip(*genotype.reduce)
+            concat = genotype.reduce_concat
+        else:
+            op_names, indices = zip(*genotype.normal)
+            concat = genotype.normal_concat
+        self._compile(C, op_names, indices, concat, reduction)
+
+    def _compile(self, C, op_names, indices, concat, reduction):
+        assert len(op_names) == len(indices)
+        def restructure(_list):
+            return [[_list[2*i],_list[2*i+1]] for i in range(len(_list)//2)]
+        op_names  = restructure(op_names)
+        indices   = restructure(indices)
+
+        self._steps     = len(op_names)
+        self._concat    = concat
+        self.multiplier = len(concat)
+        self._ops       = nn.ModuleList()
+        self.in_nodes=[]
+        for names, indexes in zip(op_names, indices):
+            in_node=[]
+            for name,index in zip(names, indexes):
+                stride = 2 if reduction and index < 2 else 1
+                if name == "deleted":continue
+                op = OPS[name](C, stride, True)
+                self._ops += [op]
+                in_node.append(index)
+            self.in_nodes.append(in_node)
+        self._indices = indices
+
+    def forward(self, s0, s1, drop_prob):
+        s0 = self.preprocess0(s0)
+        s1 = self.preprocess1(s1)
+
+        states = [s0, s1]
+        edge_id= 0
+        for in_nodes in self.in_nodes:
+            s=0
+            for in_node in in_nodes:
+                h  = states[in_node]
+                op = self._ops[edge_id]
+                h  = op(h)
+                if self.training and drop_prob > 0.0 and (not isinstance(op, Identity)):h = drop_path(h, drop_prob)
+                s+=h
+                edge_id+=1
+            states += [s]
+        return torch.cat([states[i] for i in self._concat], dim=1)
+
+
+class AuxiliaryHeadCIFAR(nn.Module):
+    def __init__(self, C, num_classes):
+        """assuming input size 8x8"""
+        super(AuxiliaryHeadCIFAR, self).__init__()
+        self.features = nn.Sequential(
+            nn.ReLU(inplace=True),
+            # image size = 2 x 2
+            nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False),
+            nn.Conv2d(C, 128, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 768, 2, bias=False),
+            nn.BatchNorm2d(768),
+            nn.ReLU(inplace=True),
+        )
+        self.classifier = nn.Linear(768, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x.view(x.size(0), -1))
+        return x
+
+
+class AuxiliaryHeadImageNet(nn.Module):
+    def __init__(self, C, num_classes):
+        """assuming input size 14x14"""
+        super(AuxiliaryHeadImageNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(5, stride=2, padding=0, count_include_pad=False),
+            nn.Conv2d(C, 128, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 768, 2, bias=False),
+            # NOTE: This batchnorm was omitted in my earlier implementation due to a typo.
+            # Commenting it out for consistency with the experiments in the paper.
+            nn.BatchNorm2d(768),
+            nn.ReLU(inplace=True),
+        )
+        self.classifier = nn.Linear(768, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x.view(x.size(0), -1))
+        return x
+
+
+class Network(nn.Module):
+    def __init__(self, C, num_classes, nodes,layers, auxiliary=False, genotype=None,**kargs):
+        super(Network, self).__init__()
+        assert genotype is not None
+        self._layers = layers
+        self._auxiliary = auxiliary
+        self.drop_path_prob = 0 ### NOTE: the origin train code will active use dropout
+        stem_multiplier = 3
+        C_curr = stem_multiplier * C
+        self.stem = nn.Sequential(
+            nn.Conv2d(1, C_curr, 3, padding=1, bias=False), nn.BatchNorm2d(C_curr)
+        )
+
+        C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
+        self.cells = nn.ModuleList()
+        reduction_prev = False
+        for i in range(layers):
+            if i in [layers // 3, 2 * layers // 3]:
+                C_curr *= 2
+                reduction = True
+            else:
+                reduction = False
+            cell = Cell(
+                genotype, C_prev_prev, C_prev, C_curr, reduction, reduction_prev
+            )
+            reduction_prev = reduction
+            self.cells += [cell]
+            C_prev_prev, C_prev = C_prev, cell.multiplier * C_curr
+            if i == 2 * layers // 3:
+                C_to_auxiliary = C_prev
+
+        if auxiliary:
+            self.auxiliary_head = AuxiliaryHeadCIFAR(C_to_auxiliary, num_classes)
+        self.global_pooling = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Linear(C_prev, num_classes)
+
+    def get_save_states(self):
+        return {
+            "state_dict": self.state_dict(),
+        }
+
+    def load_states(self, save_states):
+        self.load_state_dict(save_states["state_dict"])
+
+    def forward(self, input, **kwargs):
+        logits_aux = None
+        s0 = s1 = self.stem(input)
+        for i, cell in enumerate(self.cells):
+            s0, s1 = s1, cell(s0, s1, self.drop_path_prob)
+            if i == 2 * self._layers // 3:
+                if self._auxiliary and self.training:
+                    logits_aux = self.auxiliary_head(s1)
+        out = self.global_pooling(s1)
+        logits = self.classifier(out.view(out.size(0), -1))
+        return logits

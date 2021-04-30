8d549fd072dfc33105de1f049bf1e825a5420899
veya2ztn <zhangtianning110@gmail.com>
2021-04-30 15:52:40 +0800

    fix bug

--- torch_complex/resnet2D.py
+++ torch_complex/resnet2D.py
@@ -100,7 +100,7 @@ class ResNet(nn.Module):
         self.layer2  = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3  = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4  = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nc.AdaptiveAvgPool1d(7)
+        self.avgpool = nc.AdaptiveAvgPool2d(7)
         self.fc      = nc.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():

7acadbe4a5e8b772b290d41437ac51267c047c84
veya2ztn <zhangtianning110@gmail.com>
2021-08-09 17:04:17 +0800

    add MPSCNNlayer

--- ModelArchi/ModelSearch/operations_define.py
+++ ModelArchi/ModelSearch/operations_define.py
@@ -1,7 +1,7 @@
 from .real_operation_module import *
 from .complex_operation_module import *
 from ..SymmetryCNN import P4_Conv2d,P4Z2_Conv2d,V2_Conv2d,H2_Conv2d
-
+from ..TensorNetworkLayer.Conv2dMPS import embedConv2d
 def config_list(size,stride):
     # generate all conv type for
     record=[]
@@ -27,37 +27,46 @@ def get_OPs_for(input_dim,stride,withbasic=True,circularQ=True):
 ### all permitted operation list here
 
 OPS = {
-    "none": lambda C, stride, affine: Zero(stride),
-    "avg_pool_3x3": lambda C, stride, affine: nn.AvgPool2d(3, stride=stride, padding=1, count_include_pad=False),
-    "max_pool_3x3": lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
-    "skip_connect": lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
-    "sep_conv_3x3": lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
-    "sep_conv_5x5": lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
-    "sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
-    "dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
-    "dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
-    "conv_7x1_1x7": lambda C, stride, affine: nn.Sequential(
-        nn.ReLU(inplace=False),
-        nn.Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), bias=False),
-        nn.Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), bias=False),
-        nn.BatchNorm2d(C, affine=affine),
-    ),
-    "[cplx]sep_conv_3x3": lambda C, stride, affine: CplxSepConv(C, C, 3, stride, 1, affine=affine),
-    "[cplx]sep_conv_5x5": lambda C, stride, affine: CplxSepConv(C, C, 5, stride, 2, affine=affine),
-    "[cplx]sep_conv_7x7": lambda C, stride, affine: CplxSepConv(C, C, 7, stride, 3, affine=affine),
-    "[cplx]dil_conv_3x3": lambda C, stride, affine: CplxDilConv(C, C, 3, stride, 2, 2, affine=affine),
-    "[cplx]dil_conv_5x5": lambda C, stride, affine: CplxDilConv(C, C, 5, stride, 4, 2, affine=affine),
-    "[symP4]sep_conv_3x3": lambda C, stride, affine: sym_op.SepConv(C, C, 3, stride, 1, CNNModule=P4_Conv2d, affine=affine),
-    "[symP4]sep_conv_3x3": lambda C, stride, affine: SepConv(C, C, 3, stride, 1, CNNModule=P4_Conv2d, affine=affine),
-    "[symP4]sep_conv_5x5": lambda C, stride, affine: SepConv(C, C, 5, stride, 2, CNNModule=P4_Conv2d, affine=affine),
-    "[symP4]sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, CNNModule=P4_Conv2d, affine=affine),
-    "[symP4]dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, CNNModule=P4_Conv2d, affine=affine),
-    "[symP4]dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, CNNModule=P4_Conv2d, affine=affine),
-    "[symP4Z2]sep_conv_3x3": lambda C, stride, affine: SepConv(C, C, 3, stride, 1, CNNModule=P4Z2_Conv2d, affine=affine),
-    "[symP4Z2]sep_conv_5x5": lambda C, stride, affine: SepConv(C, C, 5, stride, 2, CNNModule=P4Z2_Conv2d, affine=affine),
-    "[symP4Z2]sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, CNNModule=P4Z2_Conv2d, affine=affine),
-    "[symP4Z2]dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, CNNModule=P4Z2_Conv2d, affine=affine),
-    "[symP4Z2]dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, CNNModule=P4Z2_Conv2d, affine=affine),
+"none": lambda C, stride, affine: Zero(stride),
+"avg_pool_3x3": lambda C, stride, affine: nn.AvgPool2d(3, stride=stride, padding=1, count_include_pad=False),
+"max_pool_3x3": lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
+"skip_connect": lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
+"sep_conv_3x3": lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
+"sep_conv_5x5": lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
+"sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
+"dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
+"dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
+"conv_7x1_1x7": lambda C, stride, affine: nn.Sequential(
+    nn.ReLU(inplace=False),
+    nn.Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), bias=False),
+    nn.Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), bias=False),
+    nn.BatchNorm2d(C, affine=affine),
+),
+
+"[cplx]sep_conv_3x3": lambda C, stride, affine: CplxSepConv(C, C, 3, stride, 1, affine=affine),
+"[cplx]sep_conv_5x5": lambda C, stride, affine: CplxSepConv(C, C, 5, stride, 2, affine=affine),
+"[cplx]sep_conv_7x7": lambda C, stride, affine: CplxSepConv(C, C, 7, stride, 3, affine=affine),
+"[cplx]dil_conv_3x3": lambda C, stride, affine: CplxDilConv(C, C, 3, stride, 2, 2, affine=affine),
+"[cplx]dil_conv_5x5": lambda C, stride, affine: CplxDilConv(C, C, 5, stride, 4, 2, affine=affine),
+
+"[symP4]sep_conv_3x3": lambda C, stride, affine: SepConv(C, C, 3, stride, 1, CNNModule=P4_Conv2d, affine=affine),
+"[symP4]sep_conv_5x5": lambda C, stride, affine: SepConv(C, C, 5, stride, 2, CNNModule=P4_Conv2d, affine=affine),
+"[symP4]sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, CNNModule=P4_Conv2d, affine=affine),
+"[symP4]dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, CNNModule=P4_Conv2d, affine=affine),
+"[symP4]dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, CNNModule=P4_Conv2d, affine=affine),
+
+"[symP4Z2]sep_conv_3x3": lambda C, stride, affine: SepConv(C, C, 3, stride, 1, CNNModule=P4Z2_Conv2d, affine=affine),
+"[symP4Z2]sep_conv_5x5": lambda C, stride, affine: SepConv(C, C, 5, stride, 2, CNNModule=P4Z2_Conv2d, affine=affine),
+"[symP4Z2]sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, CNNModule=P4Z2_Conv2d, affine=affine),
+"[symP4Z2]dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, CNNModule=P4Z2_Conv2d, affine=affine),
+"[symP4Z2]dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, CNNModule=P4Z2_Conv2d, affine=affine),
+
+"[eMPSCNN]sep_conv_3x3": lambda C, stride, affine: SepConv(C, C, 3, stride, 1, CNNModule=embedConv2d, affine=affine),
+"[eMPSCNN]sep_conv_5x5": lambda C, stride, affine: SepConv(C, C, 5, stride, 2, CNNModule=embedConv2d, affine=affine),
+"[eMPSCNN]sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, CNNModule=embedConv2d, affine=affine),
+"[eMPSCNN]dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, CNNModule=embedConv2d, affine=affine),
+"[eMPSCNN]dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, CNNModule=embedConv2d, affine=affine),
+
 
 }
 
